900e1fb5d0af9e57e0b966a93f352e1efe4dcbe1
Toaster192 <toastercz@gmail.com>
2021-01-10 21:18:51 +0100

    Add image and links to EshopList

--- src/components/EshopList/EshopList.tsx
+++ src/components/EshopList/EshopList.tsx
@@ -5,11 +5,11 @@ import { Container } from '../Container'
 import { Row } from '../Row'
 import { Col } from '../Col'
 import { FullScreenSection } from '../FullScreenSection'
-import { UnsplashCreditBadge } from '../UnsplashCreditBadge'
+import { RedditCreditBadge } from '../RedditCreditBadge'
 
 import styles from './EshopList.module.scss'
 
-import RyanSearle from './ryan-searle-345035.jpg'
+import ToasterPlanckBuild from './ToasterPlanckBuild.png'
 
 export const EshopList = () => (
   <FullScreenSection name="eshopList">
@@ -22,42 +22,42 @@ export const EshopList = () => (
               <strong>Europe:</strong>
             </p>
             <ul>
-              <li>https://keygem.store/</li>
-              <li>https://candykeys.com/</li>
-              <li>https://mykeyboard.eu/</li>
+              <li><a href="https://keygem.store/">Keygem</a></li>
+              <li><a href="https://candykeys.com/">CandyKeys</a></li>
+              <li><a href="https://mykeyboard.eu/">MyKeyboard</a></li>
             </ul>
             <p>
               <strong>Czechia:</strong>
             </p>
             <ul>
-              <li>https://42keebs.eu/</li>
+              <li><a href="https://42keebs.eu/">42keebs</a></li>
             </ul>
             <p>
               <strong>China:</strong>
             </p>
             <ul>
-              <li>https://kbdfans.com/</li>
-              <li>https://kprepublic.com/</li>
-              <li>https://www.banggood.com/</li>
-              <li>https://www.aliexpress.com/</li>
+              <li><a href="https://kbdfans.com/">Kbdfans</a></li>
+              <li><a href="https://kprepublic.com/">KP_Republic</a></li>
+              <li><a href="https://www.banggood.com/">Banggood</a></li>
+              <li><a href="https://www.aliexpress.com/">AliExpress</a></li>
             </ul>
             <p>
               <strong>US:</strong>
             </p>
             <ul>
-              <li>https://drop.com/</li>
-              <li>https://novelkeys.xyz/</li>
-              <li>https://thekey.company/</li>
+              <li><a href="https://drop.com/">Drop</a></li>
+              <li><a href="https://novelkeys.xyz/">NovelKeys</a></li>
+              <li><a href="https://thekey.company/">The Key Company</a></li>
             </ul>
           </div>
         </Col>
         <Col>
           <div className={styles.eshopListImageBox}>
-            <img alt="Ryan Searle" src={RyanSearle} />
+            <img alt="/u/toaster192" src={ToasterPlanckBuild} />
             <div className={styles.credit}>
-              <UnsplashCreditBadge
-                src="https://unsplash.com/@ryansearle?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge"
-                author="Ryan Searle"
+              <RedditCreditBadge
+                src="https://www.reddit.com/r/MechanicalKeyboards/comments/cq7ofk/photos_taking_another_20_off_my_productivity_i/"
+                author="/u/toaster192"
               />
             </div>
           </div>
